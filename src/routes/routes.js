07a808943e54c1338a52a4c43c7269bf598cb01e
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-19 23:26:45 -0500

    [Progress]: Private Router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,16 @@
-// import { useContext } from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
 import { MainContextProvider } from '../context/mainContext';
 import { LoginPage } from '../pages/_login/LoginPage';
 import { UserIdPage } from '../pages/_user/UserIdPage';
 import { UserPage } from '../pages/_user/UserPage';
 import { UserRegisterPage } from '../pages/_user/UserRegisterPage';
-// import { PrivateRoute } from './PrivateRoute';
+import { PrivateRoute } from './PrivateRoute';
 
-const Root = () => {
+const Root = (/*{value}*/) => {
+
+    // const test= useContext(MainContext);
+    // console.log(test);
+    // console.log(value)
 
     return (
         <MainContextProvider>
@@ -17,7 +20,7 @@ const Root = () => {
                 <Route path = "/user/edit/:userId" component =  { UserIdPage }/>
                 <Route path = "/user" component = { UserPage } />
                 <Redirect from = "/" to = "/login" />
-                {/* <PrivateRoute path="/" component= {LoginPage}></PrivateRoute> */}
+                {/* <PrivateRoute path="/" component= {LoginPage} isAuthenticated = { true }></PrivateRoute> */}
             </Switch>
         </MainContextProvider>
     )
